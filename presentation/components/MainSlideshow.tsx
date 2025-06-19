e0636eaa1d112cf2498f84ba1b6576814973ef77
jdreigon <jdreigon@hotmail.com>
2025-06-18 20:46:54 -0600

    se actualizaron dependencias

--- presentation/components/MainSlideshow.tsx
+++ presentation/components/MainSlideshow.tsx
@@ -1,19 +1,30 @@
 import { Movie } from '@/infrastructure/interfaces/movie.interface';
 import { useRef } from 'react';
-import { Text, View } from 'react-native';
+import { Text, useWindowDimensions, View } from 'react-native';
 import Carousel, { ICarouselInstance } from 'react-native-reanimated-carousel';
 
+import {
+    configureReanimatedLogger,
+    ReanimatedLogLevel,
+} from 'react-native-reanimated';
 
+configureReanimatedLogger({
+level: ReanimatedLogLevel.warn,
+strict: false, // Reanimated runs in strict mode by default
+});
 
 interface Props {
     movies: Movie[];
 }
 
+//Dimensions.get('screen').width solo cuando es dimensiones parado, si aumentan o boltean no lo toma en cuenta
+
+
 const MainSlideshow = ({movies}: Props) => {
 
     const ref = useRef<ICarouselInstance>(null);
-
-    console.log(movies)
+    const width = useWindowDimensions().width;
+    //console.log(movies)
 
     return(
         
@@ -29,6 +40,18 @@ const MainSlideshow = ({movies}: Props) => {
                 renderItem={ ({item}) => <Text>{item.title}</Text> }
                 width={200} //ancho de cada tarjeta
                 height={350}
+                style={{
+                    width: width,
+                    height: 350,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+                mode='parallax'   //como se mueve el carrucel
+                modeConfig={{
+                    parallaxScrollingScale: 0.9,
+                    parallaxScrollingOffset: 50,
+                }}
+                defaultIndex={1}
 
             />
             
